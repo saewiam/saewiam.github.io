b33963cf36d07ec1dbc6f3147d32fdb58bbae51a
saewiam <saewiam@gmail.com>
2025-12-03 00:06:39 -0800

    Removed line of code causing error with deleted App file, centered header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,8 @@ import { css } from "@emotion/css";
 export default function Header() {
   return (
     <header className={css`
-        
-        `}>
+      text-align: center;
+    `}>
         <h1>Marisa Cantor</h1>
     </header>
   );
